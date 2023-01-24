1bf3c6cd3d73e9bb8b9427d5b59b0a3856463819
Javan Kyalo <javankyalo2@gmail.com>
2023-01-24 23:42:32 +0300

    bots

---
+++ src/Components/YourArmyBot/yourArmyBot.js
@@ -0,0 +1,25 @@
+import "./yourArmyBot.css";
+import BotProfile from "../BotProfile/botProfile";
+import { useEffect } from "react";
+
+function YourArmyBot(props) {
+  const { army, removeFromArmy, removeFromCollection } = props;
+
+  const handleClick = (bot) => {
+    removeFromArmy(bot.id);
+  };
+
+  return (
+    <div className="bot-army">
+      <h2>Your Bot Army</h2>
+
+      <div className="bots">
+        {army.map((bot) => (
+          <BotProfile bot={bot} handleClick={handleClick} armyView={true} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default YourArmyBot;
